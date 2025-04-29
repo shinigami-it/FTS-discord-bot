d51269747d51c2c58ecbac6c4e6b29216ae09b35
DasHund007 <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>
2025-04-29 14:58:17 +0200

    fixed a minor steam news bug

---
+++ models/guild.js
@@ -0,0 +1,40 @@
+// Guild.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/database');
+
+const Guild = sequelize.define('Guild', {
+    id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+    },
+    prefix: {
+        type: DataTypes.STRING,
+        defaultValue: '!',
+    },
+    welcomeChannelId: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+    },
+    welcomeRoleId: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+    },
+    rulesId: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+    },
+    memberCountChannelId: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+    },
+    feedbackChannelId: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+    },
+}, {
+    tableName: 'guild',
+    timestamps: false,
+});
+
+module.exports = Guild;
